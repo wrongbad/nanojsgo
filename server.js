4a1bda2e97db29acc67d04c4f9cec4321a8fc613
kyle <kyle@bitcount.com>
2014-12-17 03:57:51 -0700

    major refactor. use async file load.

--- server.js
+++ server.js
@@ -2,52 +2,79 @@ var app = require('http').createServer(handler)
 var io = require('socket.io')(app);
 var fs = require('fs');
 
-var boards = {};
-var boardHashes = {};
-for(var newboard="";newboard.length<19*19;newboard+=" ");
+function isstr(s) { return (typeof s)=='string'||(s instanceof String); }
+function safestr(s) { return isstr(s) ? s.replace(/[^0-9A-Za-z\-]/g,"").substr(0,20) : ""; }
+function fnv1a(s) { for(var a=0x811c9dc5,i=0;i<s.length;i++) { a^=s.charCodeAt(i); a=a*0x193+(a<<24); } return a>>>0; }
+function contains(arr,o) { for(var i=0;i<arr.length;i++) { if(arr[i]==o) return true; } return false; }
+function get(o,k) { for(var i=0,o2={};i<k.length;i++) { o2[k[i]]=o[k[i]]; } return o2; }
+function errpre(pre) { return function(err){ if(err) console.log(pre+'::'+err); }; }
+function tojson(o) { try { return JSON.stringify(o); } catch(err) { console.log('tojson::'+err+' '+o); return null; } }
+function fromjson(o) { try { return JSON.parse(o); } catch(err) { console.log('fromjson::'+err+' '+o); return null; } }
+function opp(t) { return t=='w'?'b':'w'; }
 
-function handler(req, res) { res.writeHead(200); res.end("alive"); }
-function isstr(s) { return typeof s == 'string' || s instanceof String }
-function clean(s) { return isstr(s) ? s.replace(/[^0-9A-Za-z\-]/g,"").substr(0,20) : ""; }
-function fnv1a(s) { for(var a=0x811c9dc5,i=0;i<s.length;i++) {a^=s.charCodeAt(i);a=a*0x193+(a<<24);} return a>>>0; }
-
-function loadboard(path)
+for(var emptyboard='';emptyboard.length<19*19;emptyboard+=' ');
+function gameObj(size)
 {
-	if(!fs.existsSync(path)) return undefined;
-	var data;
-	try{ data=JSON.parse(fs.readFileSync(path,{encoding:'utf8'})); } catch(err){ console.log('load board: '+err); } 
-	try{ if(!data) require('child_process').exec('cp '+path+' '+path+'.x'); } catch(err){ console.log('copy bad board: '+err); }
-	return data ? data : undefined;
+	this.board=emptyboard,this.size=size,this.log=[],this.turn='b',this.wkills=0,this.bkills=0,this.pass=0,this.version=1;
+	this.moved=moved;
 }
+function moved(o) { 
+	if(this.log.length>3e3)
+		console.log('log>3e3'),this.log=[];
+	this.log.push(this.lmove=o);
+	this.turn=opp(this.turn); 
+	if(this.hashes) 
+		this.hashes.push(fnv1a(this.board+this.turn));
+};
 
-function saveboard(id,board)
+var migrations={
+	undefined:function(game) { migrations[0](game); },
+	0:function(game) { game.turn=game.wturn?'w':'b'; migrations[1](game); },
+	1:function(game) { game.version=1; }
+};
+
+var sendkeys=['board','lmove','size','turn','wkills','bkills','pass','bmark','wmark','qmark','mark','version'];
+var savekeys=sendkeys.concat('log');
+var games={};
+
+function loadgame(id,loaded)
 {
 	var path='boards/'+id+'.go';
-	var s;
-	if(!fs.existsSync('boards'))
-		fs.mkdirSync('boards', 0766, function(err){ if(err) console.log(err); });
-	try{ s=JSON.stringify(board); } catch(err){ console.log('save json: '+err); }
-	//console.log('save '+s+'\n to '+path+'\n');
-	if(s) fs.writeFile(path,s,{encoding:'utf8'}, function(err) { if(err) console.log('save file: '+err); });
+	fs.readFile(path,{encoding:'utf8'},function(err,data) {
+		if(err) { loaded(0); return; }
+		var game=fromjson(data);
+		if(!game) { loaded(0); return; }
+		game.moved=moved;
+		migrations[game.version](game);
+		loaded(game);
+	});
 }
 
-function getboard(socket,id)
+function savegame(id,board)
 {
-	var path="boards/"+id+".go";
-	var size=id.indexOf("9-")===0?9:id.indexOf("13-")===0?13:19;
-	if(Object.keys(boards).length>4e4) boards={};
-	var b=boards[id];
-	if(!b) b=loadboard(path);
-	if(!b) b={board:newboard,log:[],size:size,wturn:0,wkills:0,bkills:0};
-	b.log=b.log||[];
-	b.size=b.size||19;
-	b.wkills=b.wkills||0;
-	b.bkills=b.bkills||0;
-	b.pass=b.pass||0;
-	b.read=new Date().getTime();
-	for(var r in socket.rooms) socket.leave(r);
-	socket.join(id);
-	return boards[id]=b;
+	var path='boards/'+id+'.go';
+	if(!fs.existsSync('boards'))
+		fs.mkdirSync('boards',0766,errpre('mkdir'));
+	var s=tojson(get(board,savekeys));
+	fs.writeFile(path,s,{encoding:'utf8'},errpre('save'));
+}
+
+function getgame(socket,id,callback)
+{
+	if(games[id])
+		loaded(games[id]);
+	else
+		loadgame(id,loaded);
+	function loaded(game)
+	{
+		game=game||new gameObj(id.indexOf('9-')==0 ? 9 : id.indexOf('13-')==0 ? 13 : 19);
+		game.read=new Date().getTime();
+		for(var r in socket.rooms)
+			socket.leave(r);
+		socket.join(id);
+		games[id]=game;
+		callback(game);
+	}
 }
 
 function grouplib(b,s,g,lib)
@@ -67,180 +94,184 @@ function grouplib(b,s,g,lib)
 	{
 		if(g1==g2) return;
 		lib[g2]+=lib[g1];
-		for(var j=0;j<s*s;j++) if(g[j]==g1) g[j]=g2;
+		for(var i in g)
+			if(g[i]==g1)
+				g[i]=g2;
 	}	
 }
 
 var g_static=new Int16Array(19*19);
 var lib_static=new Int8Array(19*19);
-function cleardead(b,s,ord)
+function cleardead(game,b,t)
 {
+	var s=game.size;
+	var ord=t=='w'?'bw':'wb';
 	var g=g_static;
 	var lib=lib_static;
-	var pts=0;
 	grouplib(b,s,g,lib);
-	for(var i=0;i<s*s;i++)
-		if(b[i]==ord[0] && lib[g[i]]==0)
-			 b[i]=' ',pts++;
-	if(pts>0) return pts;
-	for(var i=0;i<s*s;i++)
-		if(b[i]==ord[1] && lib[g[i]]==0)
-			 b[i]=' ',pts--;
-	return pts;
+	var pts=0;
+	for(var o=0;o<2;o++)
+	{
+		for(var i=0;i<s*s;i++)
+			if(b[i]==ord[o] && lib[g[i]]==0)
+				 b[i]=' ',pts++;
+		if(pts>0)
+			return pts*(1-2*o);
+	}
+	return 0;
 }
 
-function checkko(id,board,b)
+function checkko(game,b)
 {
-	if(b.log.length<2) return 0;
+	var l=game.log;
+	if(l.length<2) return 0;
 	var ko=0;
-	var state=board.join('')+(b.wturn?'b':'w');
-	var ch=fnv1a(state);
-	if(!boardHashes[id])
+	var board=b.join('')+opp(game.turn);
+	var hash=fnv1a(board);
+	if(!game.hashes || contains(game.hashes,hash))
 	{
-		sim();
-	}
-	else
-	{
-		for(var i=0;i<boardHashes[id].length;i++)
+		var turn2='b';
+		var b2=(emptyboard+turn2).split('');
+		game.hashes=game.hashes||[];
+		for(var i=0;i<l.length;i++)
 		{
-			if(ch==boardHashes[id][i])
-			{
-				sim();
-				break;
-			}
+			if(l[i].i>=0)
+				b2[l[i].i]=l[i].v;
+			cleardead(game,b2,turn2);
+			turn2=opp(turn2);
+			b2[19*19]=turn2;
+			var board2=b2.join('');
+			ko=ko||(board2==board);
+			game.hashes[i]=game.hashes[i]||fnv1a(board2);
 		}
 	}
-	function sim()
-	{
-		var t='b';
-		s=(newboard+t).split('');
-		boardHashes[id]=[];
-		for(var i=0;i<b.log.length;i++)
-		{
-			if(b.log[i].i>=0)
-				s[b.log[i].i]=b.log[i].v;
-			if(t!=b.log[i].v)
-				console.log('log turn discrepency');
-			cleardead(s,b.size,t=='w'?'bw':'wb');
-			t=t=='w'?'b':'w';
-			s[19*19]=t;
-			str=s.join('');
-			if(str==state) ko=1;
-			boardHashes[id][i]=fnv1a(str);
-		}
-	}
-	/*
-	for(var i=0;i<boardHashes[id].length;i++)
-	console.log(i+' '+boardHashes[id][i]);
-	console.log(b.log.length+' '+ch+' '+state);
-	if(ko) console.log('ko.');
-	 */
 	return ko;
 }
 
-function trymove(id,b,i,v)
+function trymove(game,i,v)
 {
-	if(b.pass>=2) return 'over';
-	if(b.wturn&&v!='w' || !b.wturn&&v!='b') return 'turn';
-	if(!(i>=0 && i<b.size*b.size)) return 'bounds';
-	if(b.board[i]!=' ') return 'collision';
+	if(game.pass>=2) return 'over';
+	if(game.turn!==v) return 'turn';
+	if(!(i>=0 && i<game.size*game.size)) return 'bounds';
+	if(game.board[i]!=' ') return 'collision';
 	
-	newb=b.board.split('');
-	newb[i]=v;
-	var pts=cleardead(newb,b.size,b.wturn?'bw':'wb');
+	b=game.board.split('');
+	b[i]=v;
+	var pts=cleardead(game,b,game.turn);
 	if(pts<0) return 'suicide';
-	if(checkko(id,newb,b)) return 'ko';
+	if(checkko(game,b)) return 'ko';
 	
-	if(b.wturn) b.wkills+=pts;
-	if(b.bturn) b.bkills+=pts;
-	b.board=newb.join('');
-	b.wturn=!b.wturn;
-	b.pass=0;
-	if(b.qmark)
+	game.turn=='w' ? game.wkills+=pts : game.bkills+=pts;
+	game.board=b.join('');
+	game.moved({i:i,v:v});
+	game.pass=0;
+	if(game.qmark)
 	{
-		var newq=b.qmark.split('');
-		for(var i=0;i<newq.length;i++) if(newb[i]==' ') newq[i]=' ';
-		b.qmark=newq.join('');
+		var newq=game.qmark.split('');
+		for(var i in newq)
+			if(b[i]==' ')
+				newq[i]=' ';
+		game.qmark=newq.join('');
 	}
-	if(b.log.length>4e4) b.log=['overflow'];
-	b.log.push({i:i,v:v});
-	if(boardHashes[id]) boardHashes[id].push(fnv1a(b.board+(b.wturn?'w':'b')));
+	return undefined;
+}
+
+function trypass(game,v)
+{
+	if(game.turn!==v) return 'turn';
+	if(pass) return 'turn';
+	
+	game.turn=='w' ? game.bkills++ : game.wkills++;
+	game.moved({pass:1,v:game.turn});
+	game.pass++;
+	return undefined;
+}
+
+function trymark(game,v,mark)
+{
+	if(v!='w' && v!='b') return 'invalid';
+	if(!isstr(mark) || mark.length!=game.size*game.size) return 'invalid';
+	if(game.pass!=2) return 'turn';
+	
+	if(data.v=='w' && !game.wmark)
+		game.wmark=data.mark;
+	else if(data.v=='b' && !game.bmark)
+		game.bmark=data.mark;
+	else
+		return 'turn';
+	if(game.wmark && game.bmark)
+	{
+		if(game.wmark!=game.bmark)
+		{
+			var newq=game.wmark.split('');
+			for(var i in newq)
+				if(game.bmark[i]!=game.wmark[i])
+					newq[i]='?';
+			game.qmark=newq.join('');
+		}
+		else
+		{
+			game.mark=game.wmark;
+			if(game.turn=='w') 
+				game.bkills++;
+			delete game.turn;
+		}
+		game.pass=0;
+		delete game.bmark;
+		delete game.wmark;
+	}
 	return undefined;
 }
 
 app.listen(1369);
+function handler(req,res) { res.writeHead(200); res.end('alive'); }
+
 io.on('connection', function(socket) {
-	socket.on('getboard', function(data) {
-		var id=clean(data.id);
+	socket.on('get', function(data) {
+		var id=safestr(data.id);
 		if(!id) return;
-		var b=getboard(socket,id);
-		socket.emit('game', {game: b});
+		getgame(socket,id,function(game) {
+			if(!game) return;
+			socket.emit('game', {game:get(game,sendkeys)});
+		});
 	});
 	socket.on('put', function(data) {
-		var id=clean(data.id);
+		var id=safestr(data.id);
 		if(!id) return;
-		var b=getboard(socket,id);
-		var err=trymove(id,b,data.i,data.v);
-		saveboard(id,b);
-		io.to(id).emit('game', {game: b, err:err});
+		getgame(socket,id,function(game) {
+			if(!game) return;
+			var err=trymove(game,data.i,data.v);
+			savegame(id,game);
+			io.to(id).emit('game', {game:get(game,sendkeys), err:err});
+		});
 	});
 	socket.on('pass', function(data) {
-		var id=clean(data.id);
+		var id=safestr(data.id);
 		if(!id) return;
-		var b=getboard(socket,id);
-		if(b.wturn==(data.v=='w'))
-		{
-			var v=b.wturn?'w':'b';
-			b.wturn?b.bkills++:b.wkills++;
-			b.wturn=!b.wturn;
-			b.pass++;
-			b.log.push({pass:1,v:v});
-			if(boardHashes[id]) boardHashes[id].push(fnv1a(b.board+(b.wturn?'w':'b')));
-		}
-		saveboard(id,b);
-		io.to(id).emit('game', {game: b});
+		getgame(socket,id,function(game) {
+			if(!b) return;
+			var err=trypass(game,data.v);
+			savegame(id,game);
+			io.to(id).emit('game', {game:get(game,sendkeys), err:err});	  
+		});
 	});
 	socket.on('markdead', function(data) {
-		var id=clean(data.id);
+		var id=safestr(data.id);
 		if(!id) return;
-		if(!isstr(data.mark)) return;
-		var b=getboard(socket,id);
-		if(b.pass!=2) return;
-		if(data.v=='w' && !b.wmark && data.mark.length==b.size*b.size)
-			b.wmark=data.mark;
-		if(data.v=='b' && !b.bmark && data.mark.length==b.size*b.size)
-			b.bmark=data.mark;
-		if(b.bmark && b.wmark)
-		{
-			if(b.wmark!=b.bmark)
-			{
-				var q=b.wmark.split('');
-				for(var i=0;i<q.length;i++)
-					if(q[i]!=b.bmark[i])
-						q[i]='?';
-				b.qmark=q.join('');
-			}
-			else
-			{
-				b.mark=b.wmark;
-				if(b.wturn) b.bkills++;
-				b.wturn=false;
-			}
-			b.pass=0;
-			delete b.bmark;
-			delete b.wmark;
-		}
-		saveboard(id,b);
-		io.to(id).emit('game', {game: b});
+		getgame(socket,id,function(game) {
+			if(!game) return;
+			var err=trymark(game,data.v,data.mark);
+			savegame(id,game);
+			io.to(id).emit('game', {game:get(game,sendkeys), err:err});
+		});
 	});
 });
 
-setInterval(purge,5e5);
-function purge()
-{
-	if(Object.keys(boards).length<2e2) return;
-	var now=new Date().getTime();
-	for(b in boards)
-		if(now-boards[b].read>5e5)
-			delete boards[b];
-}
+setInterval(function() {
+	var ids=Object.keys(games);
+	if(ids.length<3e3) return;
+	var recent=function(a,b) { return games[a].read>games[b].read?-1:1; };
+	var keep=ids.sort(recent).slice(0,2e3);
+	games=get(games,keep);
+	console.log('cache clean');
+}, 5e5);
